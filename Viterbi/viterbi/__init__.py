4695554b2b53bc26477ef2bda57d327269fba31d
mitata11 <gregor.rogerg@live.com>
2018-07-16 12:34:50 +0100

    starting branch

--- Viterbi/viterbi/__init__.py
+++ Viterbi/viterbi/__init__.py
@@ -4,9 +4,11 @@ The package implements the model, which uses a chain of models
 for each character, as well as algorithms for training and use.
 
 This is an unfinished prototype.
+
+This branch implements a new state list abstration.
 """
 
-__version__ = '0.1 In Development'
+__version__ = '0.2 In Development'
 
 from viterbi.state import State
 from viterbi.char import Char
