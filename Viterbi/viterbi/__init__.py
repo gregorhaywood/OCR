24427db255f425ac58be4d6988b93891da90673b
mitata11 <gregor.rogerg@live.com>
2018-08-03 01:41:45 +0100

    Updated version numbers

--- Viterbi/viterbi/__init__.py
+++ Viterbi/viterbi/__init__.py
@@ -1,13 +1,7 @@
-"""This package implements OCR with a Markov model.
-
-The package implements the model, which uses a chain of models
-for each character, as well as algorithms for training and use.
-
-This is an unfinished prototype.
-This branch implements a new state list abstration.
-
+"""This package uses a Markov model to relate characters from a transcription back
+to their original image.
 """
 
-__version__ = '0.2 In Development'
+__version__ = '1.0'
 from viterbi.model import Model
 from viterbi.util import *
